ffa77fa46998e498f8b8fad2d6f5aa9e4880991a
Matthew Kasper <mkasper@andrew.cmu.edu>
2016-02-20 13:37:18 -0500

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -8,6 +8,37 @@ class Scraper(object):
     def __init__(self):
         self.url_map = dict()
 
+    #function takes html markup language and removes the <> tags from the text
+    @staticmethod
+    def removeMarkup(line):
+        removing = False
+        result = ""
+        start = 0
+        end = 0
+        length = len(line)
+        for i in range(length):
+            if(removing == False and line[i] != "<" and i != length - 1):
+                end += 1
+            elif(removing == False and line[i] == "<"):
+                removing = True
+                result += line[start : end]
+            elif(removing == True and line[i] == ">"):
+                removing = False
+                start = i + 1
+                end = i + 1
+            else:
+                if(i == length - 1):
+                    result += line[start:]
+        return result
+
+    #ensures all values in a string are printable ascii characters
+    @staticmethod
+    def isASCII(string):
+        for char in string:
+            if(ord(char) > 127):
+                return False
+        return True
+
     #returns the url generated from searching keywords on clinicaltrials.gov
     def generateURL(self, keywords):
         url = "https://clinicaltrials.gov/ct2/results?term="
@@ -18,6 +49,20 @@ class Scraper(object):
                 url = url + "+" + keywords[i]
         url += "&Search=Search"
         return url
+
+    #takes the title or link arguments passed into get_____ functions
+    #returns the URL that can be used in that function
+    #raises exception if url cannot be extracted from title or link
+    def processURL(self, title, link):
+        if(link == None and title == None):
+            raise Exception("Must pass a title or a link")
+        elif(link == None and title != None): 
+            url = self.url_map[title]
+        else: 
+            url = link
+        http = requests.get(url)
+        soup = BeautifulSoup(http.content, "html.parser")
+        return soup
 
     #returns a list of the titles in a top level keyword search
     #also maps in url_map the title to the corresponding url
@@ -44,51 +89,6 @@ class Scraper(object):
                         self.url_map[title] = new_url
         return titles
 
-    #takes the title or link arguments passed into get_____ functions
-    #returns the URL that can be used in that function
-    #raises exception if url cannot be extracted from title or link
-    def processURL(self, title, link):
-        if(link == None and title == None):
-            raise Exception("Must pass a title or a link")
-        elif(link == None and title != None): 
-            url = self.url_map[title]
-        else: 
-            url = link
-        http = requests.get(url)
-        soup = BeautifulSoup(http.content, "html.parser")
-        return soup
-
-    #function takes html markup language and removes the <> tags from the text
-    @staticmethod
-    def removeMarkup(line):
-        removing = False
-        result = ""
-        start = 0
-        end = 0
-        length = len(line)
-        for i in range(length):
-            if(removing == False and line[i] != "<" and i != length - 1):
-                end += 1
-            elif(removing == False and line [i] == "<"):
-                removing = True
-                result += line[start : end]
-            elif(removing == True and line[i] == ">"):
-                removing = False
-                start = i + 1
-                end = i + 1
-            else:
-                if(i == length - 1):
-                    result += line[start:]
-        return result
-
-    @staticmethod
-    def isASCII(string):
-        for char in string:
-            if(ord(char) > 127):
-                return False
-        return True
-
-
     #for ease of implementation these functions can work
     #whether given a title it's seen before or a url to parse
     def getPurpose(self, title = None, link = None):
@@ -102,7 +102,7 @@ class Scraper(object):
             line = self.removeMarkup(html[i])
             if(self.isASCII(line)):
                 result += line + "\n"
-        return result
+        return result.strip()
     
     #returns (start, end) of study / (None, None) if no date found        
     def getTimeFrame(self, title = None, link = None):
@@ -111,7 +111,6 @@ class Scraper(object):
         start = self.getStart(soup, html)
         end = self.getEnd(soup, html)
         return (start, end)
-        
 
     def getStart(self, soup, html):
         length = len(html)
@@ -120,7 +119,7 @@ class Scraper(object):
             if(html[i].find("Start Date") != -1):
                 start_index = i + 1
         if(start_index == None): return None
-        return self.removeMarkup(html[start_index])
+        return self.removeMarkup(html[start_index]).strip()
 
     def getEnd(self, soup, html):
         length = len(html)
@@ -133,8 +132,7 @@ class Scraper(object):
         date = (result.split(" "))
         if(len(date) > 2):
             result = date[0] + " " + date[1]
-        return result
-
+        return result.strip()
 
     def getCriteria(self, title = None, link = None):
         soup = self.processURL(title, link)
@@ -152,18 +150,19 @@ class Scraper(object):
                 break
             if(self.isASCII(line) and start == True):
                 result += line + "\n"
-        return result
+        return result.strip()
 
-    def getDescription(self, title = None, link = None):
-        soup = self.processURL(title, link)
-
-
-
-#keywords = ["cancer", "diabetes", "cholesterol"]
-#x = Scraper()
-#titles = x.searchAllStudies(keywords)
-#print(x.url_map[titles[5]])
-#print(x.getTimeFrame(title = titles[6]))
-#print(x.url_map[titles[3]])
-#print(x.getPurpose(titles[3]))
-#print(x.getCriteria(titles[8]))
+keywords = ["trisomy", "21"]
+x = Scraper()
+titles = x.searchAllStudies(keywords)
+i = 5
+#title
+print(titles[i], end = "\n\n")
+#URL
+print(x.url_map[titles[i]], end = "\n\n")
+#START AND END DATE
+print(x.getTimeFrame(title = titles[i]), end = "\n\n")
+#PURPOSE
+print(x.getPurpose(titles[i]), end = "\n\n")
+#INCLUSION CRITERIA
+print(x.getCriteria(titles[i]), end = "\n\n")

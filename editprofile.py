2ca92b679df534164f3ec2bf7d3e1a0537a54649
Scott Hamal <scotthamal1@gmail.com>
2016-02-20 23:29:27 -0500

    Added files via upload

--- editprofile.py
+++ editprofile.py
@@ -12,6 +12,7 @@ from tkinter import *
 ####################################
 
 def init(data):
+    data.ground = PhotoImage(file="ground.gif")
     data.image = PhotoImage(file="smalogo.gif")
     print(data.image)
     data.conditionEntry = Entry(data.root, font ="Helvetica 44",text="poo", bd =5)
@@ -20,15 +21,25 @@ def init(data):
     data.profiles = interperateProfiles("profiles.txt")
 
 def mousePressed(event, data):
-    # for i in range(8):
-    #     print(event.x, event.y, i)
-    #     print(380<event.x<410, "x")
-    #     print(260+50*(i) , 230+50*(i), "y", event.y)
-    #     print(260+50*(i) < event.y < 230+50*(i))
-    #     if(380 < event.x < 410 and (260+50*(i)) < event.y < (230+50*(i))):
-    #         print("ya")
-    #         data.profiles.pop(i)
-    pass
+    for i in range(8):
+        # print(event.x, event.y, i)
+        # print(380<event.x<410, "x")
+        # print(260+50*(i) , 230+50*(i), "y", event.y)
+        # print(260+50*(i) < event.y < 230+50*(i))
+        if(380 < event.x < 410 and (230+50*(i)) < event.y < (260+50*(i))):
+            try:
+                data.profiles[data.currentUser].pop(i)
+                writeFile("profiles.txt", stringProfiles(data.profiles))
+            except:
+                pass
+        if(380+425 < event.x < 410+425 and (230+50*(i)) < event.y < (260+50*(i))):
+            # print("ya")
+            try:
+                data.profiles[data.currentUser].pop(i+8)
+                writeFile("profiles.txt", stringProfiles(data.profiles))
+            except:
+                pass
+
 
 def keyPressed(event, data):
     # use event.char and event.keysym
@@ -41,10 +52,13 @@ def keyPressed(event, data):
 def timerFired(data):
     pass
 
+
+
 def redrawAll(canvas, data):
     canvas.create_rectangle(0,0,data.width,data.height, fill="black")
+    # canvas.create_image(data.width/2,data.height/2,image=data.ground)
     canvas.create_image(660,100, image=data.image)
-    canvas.create_text(30,80,font="Helvetica 44", fill="white", text="My Profile -- " + data.currentUser, anchor="sw")
+    canvas.create_text(30,80,font="Helvetica 44", fill="white", text="My Profile  -  " + data.currentUser, anchor="sw")
     canvas.create_text(30,140,font="Helvetica 24", fill="white",text="Add a Topic of Interest:", anchor="sw")
     canvas.create_window(30,215, window= data.conditionEntry, anchor="sw")    
     for x in range(len(data.profiles[data.currentUser])):
